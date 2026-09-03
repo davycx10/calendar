d9b3bb636379abe1df32309ef85324ffc5707103
davycx <davycx.com>
2026-09-03 19:48:32 +0200

    correction function zeller to calculate start day

--- c++/cpp/src/main.cpp
+++ c++/cpp/src/main.cpp
@@ -11,7 +11,7 @@ int get_start_day(int month, int year){
     }
     int a = year % 100;
     int b = year / 100;
-    int start_day = (1 + ((12 * (month + 1)) / 5) + a + (a / 4) + (b / 4) - (2 * b)) % 7; 
+    int start_day = (1 + ((13 * (month + 1)) / 5) + a + (a / 4) + (b / 4) - (2 * b)) % 7; 
 
     return start_day;
 }
@@ -20,7 +20,7 @@ int get_start_day(int month, int year){
 int main(int argc, char const *argv[])
 {
     
-    int start_day = get_start_day(1, 2023);
+    int start_day = get_start_day(9, 2026);
     std::cout << "Start day: " << start_day << std::endl;
     return 0;
 }
